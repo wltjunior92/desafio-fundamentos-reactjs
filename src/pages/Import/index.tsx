6feb82916d0db1d2658a09887aeeb76d4dfe3247
Walter Junior <wlt.junior92@gmail.com>
2020-04-24 10:18:10 -0300

    Solving the challenge

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,27 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
+    const data = new FormData();
     try {
-      // await api.post('/transactions/import', data);
+      uploadedFiles.forEach(file => {
+        data.append('file', file.file);
+      });
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    // eslint-disable-next-line array-callback-return
+    files.map((file): void => {
+      const newFile: FileProps = {
+        file,
+        name: file.name,
+        readableSize: file.size.toString(),
+      };
+      setUploadedFiles([...uploadedFiles, newFile]);
+    });
   }
 
   return (
